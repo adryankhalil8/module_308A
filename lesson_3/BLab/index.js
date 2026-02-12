1f47ef13fb12b9237bec33451fc07e2397f70c74
adryankhalil8 <adryan.jefferson@peopleshores.com>
2026-02-12 10:36:25 -0500

    Lesson 3 complete

--- lesson_3/BLab/index.js
+++ lesson_3/BLab/index.js
@@ -1,10 +1,38 @@
-console.log("--GLAB--");
-import { central, db1, db2, db3, vault } from "./databases.js";
-
-function getUserData(id) {
-  const dbs = {
-    db1: db1,
-    db2: db2,
-    db3: db3
-  };
+//importing database functions
+import {central, db1, db2, db3, vault} from "./database.js";
+ 
+//main function
+export async function getUserData(id) {
+ 
+    //step 1: validate input
+    if(typeof id !== "number") {
+        throw new Error("Invalid Input -- Not a Number!");
+    }
+    if (id < 1 || id > 10) {
+        throw new Error("Invalid Input -- Out of Range");
+    }
+    //Step 2: Create database lookup object
+    const dbs = {
+        db1,
+        db2,
+        db3
+    };
+    //Step 3: Ask central which databse to use
+    const dbName = await central(id);
+    // dbs[dbName](id)
+    //Step 4: Run correct everything into one object
+    const [basicInfo, personalInfo] = await Promise.all([
+        dbs[dbName](id),
+        vault(id)
+    ]);
+    //Step 5: combine everything into one object
+    return {
+        id,
+        ...personalInfo,
+        ...basicInfo
+    };
 }
+ getUserData(5)
+ 
+.then(data => console.log("valid User:", data))
+.catch(err => console.error("Error:", err. Message));
